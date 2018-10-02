8ada5e302539a2d96b3fe73ac7982d7ae943e146
alex-alspaw <alspawalex@gmail.com>
2018-10-02 14:33:16 -0500

    add weather overlay

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,307 @@
+var map;
+var geoJSON;
+var request;
+var gettingData = false;
+var openWeatherMapKey = "73c3d994dd080efa8f6beab2a4662696"
+function initialize() {
+    var mapOptions = {
+        zoom: 4,
+        center: new google.maps.LatLng(50, -50)
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+    // Add interaction listeners to make weather requests
+    google.maps.event.addListener(map, 'idle', checkIfDataRequested);
+    // Sets up and populates the info window with details
+    map.data.addListener('click', function (event) {
+        infowindow.setContent(
+            "<img src=" + event.feature.getProperty("icon") + ">"
+            + "<br /><strong>" + event.feature.getProperty("city") + "</strong>"
+            + "<br />" + event.feature.getProperty("temperature") + "&deg;C"
+            + "<br />" + event.feature.getProperty("weather")
+        );
+        infowindow.setOptions({
+            position: {
+                lat: event.latLng.lat(),
+                lng: event.latLng.lng()
+            },
+            pixelOffset: {
+                width: 0,
+                height: -15
+            }
+        });
+        infowindow.open(map);
+    });
+}
+var checkIfDataRequested = function () {
+    // Stop extra requests being sent
+    while (gettingData === true) {
+        request.abort();
+        gettingData = false;
+    }
+    getCoords();
+};
+// Get the coordinates from the Map bounds
+var getCoords = function () {
+    var bounds = map.getBounds();
+    var NE = bounds.getNorthEast();
+    var SW = bounds.getSouthWest();
+    getWeather(NE.lat(), NE.lng(), SW.lat(), SW.lng());
+};
+// Make the weather request
+var getWeather = function (northLat, eastLng, southLat, westLng) {
+    gettingData = true;
+    var requestString = "http://api.openweathermap.org/data/2.5/box/city?bbox="
+        + westLng + "," + northLat + "," //left top
+        + eastLng + "," + southLat + "," //right bottom
+        + map.getZoom()
+        + "&cluster=yes&format=json"
+        + "&APPID=" + openWeatherMapKey;
+    request = new XMLHttpRequest();
+    request.onload = proccessResults;
+    request.open("get", requestString, true);
+    request.send();
+};
+// Take the JSON results and proccess them
+var proccessResults = function () {
+    console.log(this);
+    var results = JSON.parse(this.responseText);
+    if (results.list.length > 0) {
+        resetData();
+        for (var i = 0; i < results.list.length; i++) {
+            geoJSON.features.push(jsonToGeoJson(results.list[i]));
+        }
+        drawIcons(geoJSON);
+    }
+};
+var infowindow = new google.maps.InfoWindow();
+// For each result that comes back, convert the data to geoJSON
+var jsonToGeoJson = function (weatherItem) {
+    var feature = {
+        type: "Feature",
+        properties: {
+            city: weatherItem.name,
+            weather: weatherItem.weather[0].main,
+            temperature: weatherItem.main.temp,
+            min: weatherItem.main.temp_min,
+            max: weatherItem.main.temp_max,
+            humidity: weatherItem.main.humidity,
+            pressure: weatherItem.main.pressure,
+            windSpeed: weatherItem.wind.speed,
+            windDegrees: weatherItem.wind.deg,
+            windGust: weatherItem.wind.gust,
+            icon: "http://openweathermap.org/img/w/"
+                + weatherItem.weather[0].icon + ".png",
+            coordinates: [weatherItem.coord.Lon, weatherItem.coord.Lat]
+        },
+        geometry: {
+            type: "Point",
+            coordinates: [weatherItem.coord.Lon, weatherItem.coord.Lat]
+        }
+    };
+    // Set the custom marker icon
+    map.data.setStyle(function (feature) {
+        return {
+            icon: {
+                url: feature.getProperty('icon'),
+                anchor: new google.maps.Point(25, 25)
+            }
+        };
+    });
+    // returns object
+    return feature;
+};
+// Add the markers to the map
+var drawIcons = function (weather) {
+    map.data.addGeoJson(geoJSON);
+    // Set the flag to finished
+    gettingData = false;
+};
+// Clear data layer and geoJSON
+var resetData = function () {
+    geoJSON = {
+        type: "FeatureCollection",
+        features: []
+    };
+    map.data.forEach(function (feature) {
+        map.data.remove(feature);
+    });
+};
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 // DOCUMENT START
-// INPUT ON-CLICK FUNCTION
-// INITIAL MAPS API
+let map;
+
+let geoJSON;
+let request;
+let gettingData = false;
+
+const openWeatherMapKey = '73c3d994dd080efa8f6beab2a4662696';
+
+let weatherLayerURL = `https://tile.openweathermap.org/map/{layer}/{z}/{x}/{y}.png?appid=${openWeatherMapKey}`;
+
+let directionsURL = `https://maps.googleapis.com/maps/api/directions/outputFormat?parameters`;
+
+
+
+
+    let westLng = '80';
+    let northLat = '70';
+    let eastLng = '60';
+    let southLat = '30';
+
+
+
+    let weatherURL = "http://api.openweathermap.org/data/2.5/box/city?bbox="
+        + westLng + "," + northLat + "," //left top
+        + eastLng + "," + southLat + "," //right bottom
+        + map.getZoom()
+        + "&cluster=yes&format=json"
+        + "&APPID=" + openWeatherMapKey;
+
+
+
+
+
+    $.ajax({
+
+        url: weatherURL,
+        method: 'GET'
+
+    }).then(function (response) {
+
+
+
+
+        console.log(response);
+        var results = JSON.parse(response);
+        if (results.list.length > 0) {
+            resetData();
+            for (var i = 0; i < results.list.length; i++) {
+                geoJSON.features.push(jsonToGeoJson(results.list[i]));
+            }
+            drawIcons(geoJSON);
+        }
+
+
+
+        var jsonToGeoJson = function (weatherItem) {
+            var feature = {
+                type: "Feature",
+                properties: {
+                    city: weatherItem.name,
+                    weather: weatherItem.weather[0].main,
+                    temperature: weatherItem.main.temp,
+                    min: weatherItem.main.temp_min,
+                    max: weatherItem.main.temp_max,
+                    humidity: weatherItem.main.humidity,
+                    pressure: weatherItem.main.pressure,
+                    windSpeed: weatherItem.wind.speed,
+                    windDegrees: weatherItem.wind.deg,
+                    windGust: weatherItem.wind.gust,
+                    icon: "http://openweathermap.org/img/w/"
+                        + weatherItem.weather[0].icon + ".png",
+                    coordinates: [weatherItem.coord.Lon, weatherItem.coord.Lat]
+                },
+                geometry: {
+                    type: "Point",
+                    coordinates: [weatherItem.coord.Lon, weatherItem.coord.Lat]
+                }
+            };
+            // Set the custom marker icon
+            map.data.setStyle(function (feature) {
+                return {
+                    icon: {
+                        url: feature.getProperty('icon'),
+                        anchor: new google.maps.Point(25, 25)
+                    }
+                };
+            });
+            // returns object
+            return feature;
+        };
+
+
+
+    });
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+
+    // INPUT ON-CLICK FUNCTION
+
+
+    google.maps.event.addEventListener(map, 'idle', checkIfDataRequested);
+
+    map.data.addEventListener('click', function (event) {
+        infowindow.setContent(
+            "<img src=" + event.feature.getProperty("icon") + ">"
+            + "<br /><strong>" + event.feature.getProperty("city") + "</strong>"
+            + "<br />" + event.feature.getProperty("temperature") + "&deg;C"
+            + "<br />" + event.feature.getProperty("weather")
+        );
+        infowindow.setOptions({
+            position: {
+                lat: event.latLng.lat(),
+                lng: event.latLng.lng()
+            },
+            pixelOffset: {
+                width: 0,
+                height: -15
+            }
+        });
+        infowindow.open(map);
+    });
+
+
+
+    //$.ajax({
+    //
+    //    url: weatherLayerURL,
+    //      method: 'GET'
+
+    //}).then(function (response) {
+
+
+
+
+
+});
 // WEATHER OVERLAY API
+
+
 // GEOLOCATION API
 // CURRENT FORECAST API
 // DAILY/5 DAY FORECAST API
@@ -9,4 +309,4 @@
 // TIME ZONE API
 // DISTANCE MATRIX API
 // DIRECTIONS API
-// ROADS API
+// ROADS API*/
