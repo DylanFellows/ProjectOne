eedc0a495a0d4cb53459342c51c1423f72c29112
Akinoe <dylan101fellows@yahoo.com>
2018-10-03 13:17:39 -0500

    zoom commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,9 +3,10 @@ var map;
   var request;
   var gettingData = false;
   var openWeatherMapKey = "73c3d994dd080efa8f6beab2a4662696"
+ 
   function initialize() {
     var mapOptions = {
-      zoom: 4,
+      zoom: 6,
       center: new google.maps.LatLng(50,-50)
     };
     map = new google.maps.Map(document.getElementById('map-canvas'),
@@ -17,7 +18,7 @@ var map;
       infowindow.setContent(
        "<img src=" + event.feature.getProperty("icon") + ">"
        + "<br /><strong>" + event.feature.getProperty("city") + "</strong>"
-       + "<br />" + event.feature.getProperty("temperature") + "&deg;C"
+       + "<br />" + event.feature.getProperty("temperature") + "&deg;F"
        + "<br />" + event.feature.getProperty("weather")
        );
       infowindow.setOptions({
@@ -54,7 +55,7 @@ var map;
     var requestString = "http://api.openweathermap.org/data/2.5/box/city?bbox="
                         + westLng + "," + northLat + "," //left top
                         + eastLng + "," + southLat + "," //right bottom
-                        + map.getZoom()
+                        + map.getZoom() + "&units=imperial"
                         + "&cluster=yes&format=json"
                         + "&APPID=" + openWeatherMapKey;
     request = new XMLHttpRequest();
@@ -67,9 +68,6 @@ var map;
     console.log(this);
     var results = JSON.parse(this.responseText);
     if (results.list.length > 0) {
-      console.log(results);
-      console.log(results.list);
-      console.log(results.list.length);
         resetData();
         for (var i = 0; i < results.list.length; i++) {
           geoJSON.features.push(jsonToGeoJson(results.list[i]));
@@ -131,3 +129,25 @@ var map;
     });
   };
   google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
+$('#btnSubmit').on('click', function(event){
+  event.preventDefault();
+  var cityCountry = $('#srcinpt').val();
+  console.log(cityCountry);
+  $.ajax({
+    url: `http://api.openweathermap.org/data/2.5/forecast?q=${cityCountry}&appid=${openWeatherMapKey}`,
+    method: 'GET'
+  }).then(function(response){
+    console.log(response);
+    moveToLocation(response.city.coord.lat, response.city.coord.lon);
+    $('#srcinpt').val('');
+  });
+});
+
+function moveToLocation(lat, lng){
+  var center = new google.maps.LatLng(lat, lng);
+  map.panTo(center);
+}
