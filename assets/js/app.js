7e103efbc906d8b182c0e89c53e330a11a6aa328
alex-alspaw <alspawalex@gmail.com>
2018-10-05 10:00:52 -0500

    autocomplete

--- assets/js/app.js
+++ assets/js/app.js
@@ -39,7 +39,7 @@ function showPosition(position) {
         x.innerHTML = "Current Address: " + response.results[0].formatted_address;
         // +
         //"<br>Longitude: " + position.coords.longitude;
-        console.log(response.results[0].formatted_address);
+        //console.log(response.results[0].formatted_address);
 
     });
 
@@ -203,6 +203,103 @@ var resetData = function () {
     });
 };
 google.maps.event.addDomListener(window, 'load', initialize);
+/*
+var autocomplete = new google.maps.places.Autocomplete($("#srcinput"));
+
+autocomplete.bindTo('bounds', map);
+autocomplete.setFields(['address_components', 'geometry', 'icon', 'name']);
+
+
+var infowindow = new google.maps.InfoWindow();
+var infowindowContent = document.getElementById('map-canvas');
+infowindow.setContent(infowindowContent);
+//var marker = new google.maps.Marker({
+//  map: map,
+//anchorPoint: new google.maps.Point(0, -29)
+//});
+
+var address = '';
+if (place.address_components) {
+    address = [
+        (place.address_components[0] && place.address_components[0].short_name || ''),
+        (place.address_components[1] && place.address_components[1].short_name || ''),
+        (place.address_components[2] && place.address_components[2].short_name || '')
+    ].join(' ');
+}
+
+
+infowindowContent.children['place-address'].textContent = address;
+
+
+
+
+autocomplete.addListener('place_changed', function () {
+    infowindow.close();
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+        // User entered the name of a Place that was not suggested and
+        // pressed the Enter key, or the Place Details request failed.
+        window.alert("No details available for input: '" + place.name + "'");
+        return;
+    }
+
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+    } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);  // Why 17? Because it looks good.
+    }
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+
+    var address = '';
+    if (place.address_components) {
+        address = [
+            (place.address_components[0] && place.address_components[0].short_name || ''),
+            (place.address_components[1] && place.address_components[1].short_name || ''),
+            (place.address_components[2] && place.address_components[2].short_name || '')
+        ].join(' ');
+    }
+
+    infowindowContent.children['place-icon'].src = place.icon;
+    infowindowContent.children['place-name'].textContent = place.name;
+    infowindowContent.children['place-address'].textContent = address;
+    infowindow.open(map, marker);
+});
+
+// Sets a listener on a radio button to change the filter type on Places
+// Autocomplete.
+function setupClickListener(id, types) {
+    var radioButton = document.getElementById(id);
+    radioButton.addEventListener('click', function () {
+        autocomplete.setTypes(types);
+    });
+}
+
+setupClickListener('changetype-all', []);
+setupClickListener('changetype-address', ['address']);
+setupClickListener('changetype-establishment', ['establishment']);
+setupClickListener('changetype-geocode', ['geocode']);
+
+document.getElementById('use-strict-bounds')
+    .addEventListener('click', function () {
+        console.log('Checkbox clicked! New state=' + this.checked);
+        autocomplete.setOptions({ strictBounds: this.checked });
+    });*/
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 $('#btnSubmit').on('click', function (event) {
@@ -215,27 +312,38 @@ $('#btnSubmit').on('click', function (event) {
 
     $.ajax({
 
-        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${destAddress}&key=AIzaSyD2tX38tR0PVZxcCq_jSiPvpTcG-JrV1qk`,
+        url: `https://maps.googleapis.com/maps/api/place/autocomplete/json?address=${destAddress}&key=AIzaSyD2tX38tR0PVZxcCq_jSiPvpTcG-JrV1qk`,
+        //url: `https://maps.googleapis.com/maps/api/geocode/json?address=${destAddress}&key=AIzaSyD2tX38tR0PVZxcCq_jSiPvpTcG-JrV1qk`,
         method: 'GET'
 
     }).then(function (response) {
+        $('#btnSubmit').keypress(function () {
 
+            $.ajax({
+
+                url: `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${destAddress}&key=AIzaSyD2tX38tR0PVZxcCq_jSiPvpTcG-JrV1qk`,
+                method: 'GET'
+
+            }).then(function (response) {
+
+            });
+        });
         console.log(response.results[0].geometry.location);
         moveToLocation(response.results[0].geometry.location.lat, response.results[0].geometry.location.lng);
         $('#srcinpt').val('');
-
     });
-
 });
 
+
+
 function moveToLocation(lat, lng) {
-
-
     var center = new google.maps.LatLng(lat, lng);
     map.panTo(center);
-
-
 };
+
+
+
+
 
 /*   Leave this in for now, just in case we need to do an ajax call to reverse GeoCode
 $.ajax({
@@ -247,4 +355,5 @@ $.ajax({
 
     console.log(response.results[0].formatted_address);
 
-});*/
+});
+*/
